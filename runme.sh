6f5b536021018ed82344ebf6abcc8ceeb7fe79ee
reekol <cupuyc@gmail.com>
2019-02-22 19:33:37 +0200

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -2,8 +2,8 @@
 
 sudo apt-get install git build-essential linux-headers-generic
 git clone https://github.com/abperiasamy/rtl8812AU_8821AU_linux.git
-cp D-Link-DWA-172.patch ./rtl8812au
-cd rtl8812au
+cp D-Link-DWA-172.patch ./rtl8812AU_8821AU_linux
+cd rtl8812AU_8821AU_linux
 git apply D-Link-DWA-172.patch
 make
 sudo make install
